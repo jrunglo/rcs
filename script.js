79c7b8850926e98733ad87797006ceb776d0cc07
jrunglo <119487123+jrunglo@users.noreply.github.com>
2022-12-01 11:16:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// ---------horizontal-navbar-menu-----------
+var tabsNewAnim = $('#navbar-animmenu');
+var selectorNewAnim = $('#navbar-animmenu').find('li').length;
+//var selectorNewAnim = $(".tabs").find(".selector");
+var activeItemNewAnim = tabsNewAnim.find('.active');
+var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+var itemPosNewAnimLeft = activeItemNewAnim.position();
+$(".hori-selector").css({
+    "left":itemPosNewAnimLeft.left + "px",
+    "width": activeWidthNewAnimWidth + "px"
+});
+$("#navbar-animmenu").on("click","li",function(e){
+    $('#navbar-animmenu ul li').removeClass("active");
+    $(this).addClass('active');
+    var activeWidthNewAnimWidth = $(this).innerWidth();
+    var itemPosNewAnimLeft = $(this).position();
+    $(".hori-selector").css({
+        "left":itemPosNewAnimLeft.left + "px",
+        "width": activeWidthNewAnimWidth + "px"
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
